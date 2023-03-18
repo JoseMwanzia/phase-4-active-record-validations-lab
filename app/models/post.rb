2e4527ac7f6fec46124c040eb8196f42c78c2abe
joseph mwanzia <makamwanzia@gmail.com>
2023-03-18 17:13:58 +0300

    Completed lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :custom_validations
+     
+    def custom_validations
+        if title == "True Facts"
+            errors.add(:title, "Invalid title")
+        end
+    end
+
 end
